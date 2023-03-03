e9a696513f5d0cdb5cf0881d273eeb30d619528b
ayanshnamdeo <ayansh.namdeo@nagarro.com>
2023-03-03 17:59:24 +0530

    demo

---
+++ src/main/java/com/nagarro/controller/DockerController.java
@@ -0,0 +1,16 @@
+package com.nagarro.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/docker")
+public class DockerController {
+	
+	@GetMapping("/all")
+	public String getAllData() {
+		return "yeah Docker is working!!!!";
+	}
+
+}
---
+++ src/main/java/com/nagarro/exception/DockerException.java
@@ -0,0 +1,11 @@
+package com.nagarro.exception;
+
+public class DockerException  extends Exception {
+	
+	private static final long serialVersionUID = 1L;
+	
+		public DockerException(String message) {
+			super(message);
+		}
+
+}
